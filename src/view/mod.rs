6af216170022877278fafc1fc750cc4c9c8a6098
tipsypastels <dakotasankey@gmail.com>
2026-02-13 09:01:03 -0800

    split some fns

--- src/view/mod.rs
+++ src/view/mod.rs
@@ -3,17 +3,20 @@ mod bbcode;
 mod css;
 mod unit;
 
-use self::{
-    bbcode::BBCode,
-    css::Css,
-    unit::{absolute, color},
+use self::{bbcode::BBCode, css::Css, unit::absolute};
+use crate::{
+    model::{
+        MapFind,
+        room::{Room, RoomMember},
+    },
+    state::State,
 };
-use crate::{model::MapFind, state::State};
 use anyhow::Result;
 use assets::{egg_asset, pokemon_asset, room_asset};
 
 const SPARKLE: &str = "url('/images/sparkle.gif')";
 const TABS_WIDTH: usize = 500;
+const TABS_HEADER_PADDING: &str = "0.25rem 0.5rem";
 
 pub fn render(state: &State) -> Result<String> {
     let mut bb = BBCode::new();
@@ -21,78 +24,96 @@ pub fn render(state: &State) -> Result<String> {
     bb.tag_with_multi(
         "tabs",
         (("width", TABS_WIDTH), ("block_align", "bcenter")),
-        |bb| render_room(bb, state),
+        |bb| render_rooms(bb, state),
     )?;
 
     Ok(bb.to_string())
 }
 
-pub fn render_room(bb: &mut BBCode, state: &State) -> Result<()> {
+pub fn render_rooms(bb: &mut BBCode, state: &State) -> Result<()> {
     for room in state.src.rooms.values() {
-        bb.tag("slide_header", |bb| {
-            bb.tag_with(
-                "div",
-                Css::new()
-                    .set("margin", "0.25rem 0.5rem")
-                    .set("font-weight", "bold")
-                    .set("color", color(room.color)),
-                |bb| {
-                    bb.text(&room.name);
-                },
-            );
+        render_room(bb, state, room)?;
+    }
+    Ok(())
+}
+
+fn render_room(bb: &mut BBCode, state: &State, room: &Room) -> Result<()> {
+    bb.tag("slide_header", |bb| {
+        bb.tag_with("div", Css::new().set("margin", TABS_HEADER_PADDING), |bb| {
+            bb.text(&room.name);
         });
+    });
 
-        bb.tag("slide", |bb| {
-            bb.tag_with("div", Css::new().set("position", "relative"), |bb| {
-                bb.tag_with("cimg", "user-select:none", |bb| {
-                    bb.text(room_asset(&room.key));
-                });
+    bb.tag("slide", |bb| {
+        bb.tag_with("div", Css::new().set("position", "relative"), |bb| {
+            bb.tag_with("cimg", "user-select:none", |bb| {
+                bb.text(room_asset(&room.key));
+            });
 
-                for (placement_i, placement) in room.pokemon.values().enumerate() {
-                    let tab_no = placement_i + 1;
-
-                    let pokemon = state.src.pokemon.find(&placement.key)?;
-                    let growth = state.growth_state(pokemon)?;
-                    let species = growth.species_key();
-
-                    let sprite_css = Css::new()
-                        .set("position", "absolute")
-                        .setopt(placement.flipped, "transform", "scaleX(-1)")
-                        .setopt(pokemon.shiny, "background-image", SPARKLE)
-                        .extend(absolute(placement.position.unpack()));
-
-                    bb.tag_with("title", &pokemon.name, |bb| {
-                        bb.tag_with_unquoted("tab", tab_no, |bb| {
-                            bb.tag_with("cimg", sprite_css, |bb| {
-                                if let Some(species) = species {
-                                    bb.text(pokemon_asset(&pokemon.key, species));
-                                } else {
-                                    bb.text(egg_asset());
-                                }
-                            });
-                        });
-                    });
-                }
-
-                Ok(())
-            })?;
-
-            bb.tag_with_multi("tabs", (("width", "100%"),), |bb| {
-                for placement in room.pokemon.values() {
-                    let pokemon = state.src.pokemon.find(&placement.key)?;
-
-                    bb.tag("slide_header", |bb| {
-                        bb.text(&pokemon.name);
-                    })
-                    .tag("slide", |bb| {
-                        bb.text(format!("{pokemon:?}"));
-                    });
-                }
-                Ok(())
-            })?;
+            for (member_i, member) in room.pokemon.values().enumerate() {
+                render_room_member_sprite(bb, state, room, member_i, member)?;
+            }
 
             Ok(())
         })?;
-    }
+
+        bb.tag_with_multi("tabs", (("width", "100%"),), |bb| {
+            for member in room.pokemon.values() {
+                render_room_member_info(bb, state, member)?;
+            }
+            Ok(())
+        })?;
+
+        Ok(())
+    })?;
+
+    Ok(())
+}
+
+fn render_room_member_sprite(
+    bb: &mut BBCode,
+    state: &State,
+    room: &Room,
+    i: usize,
+    member: &RoomMember,
+) -> Result<()> {
+    let tab_no = i + 1;
+    let pokemon = state.src.pokemon.find(&member.key)?;
+    let growth = state.growth_state(pokemon)?;
+    let species = growth.species_key();
+
+    let sprite_css = Css::new()
+        .set("position", "absolute")
+        .setopt(member.flipped, "transform", "scaleX(-1)")
+        .setopt(pokemon.shiny, "background-image", SPARKLE)
+        .extend(absolute(member.position.unpack()));
+
+    bb.tag_with("title", &pokemon.name, |bb| {
+        bb.tag_with_unquoted("tab", tab_no, |bb| {
+            bb.tag_with("cimg", sprite_css, |bb| {
+                if let Some(species) = species {
+                    bb.text(pokemon_asset(&pokemon.key, species));
+                } else {
+                    bb.text(egg_asset());
+                }
+            });
+        });
+    });
+
+    Ok(())
+}
+
+fn render_room_member_info(bb: &mut BBCode, state: &State, member: &RoomMember) -> Result<()> {
+    let pokemon = state.src.pokemon.find(&member.key)?;
+
+    bb.tag("slide_header", |bb| {
+        bb.tag_with("div", Css::new().set("margin", TABS_HEADER_PADDING), |bb| {
+            bb.text(&pokemon.name);
+        });
+    })
+    .tag("slide", |bb| {
+        bb.text(format!("{pokemon:?}"));
+    });
+
     Ok(())
 }
